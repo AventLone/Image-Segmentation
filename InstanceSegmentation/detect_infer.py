c566441475fadb2d7dea7db75caf7a8a73b22e86
Avent Lone <piggy.stoic@outlook.com>
2026-06-21 09:50:38 +0800

    :hammer: Update

--- InstanceSegmentation/detect_infer.py
+++ InstanceSegmentation/detect_infer.py
@@ -1,10 +1,9 @@
-import supervision as sv
-from rfdetr import RFDETRSegMedium, RFDETRSegLarge
-from PIL import Image, ImageOps
+from rfdetr import RFDETRMedium
+from PIL import Image
 import cv2
 import numpy as np
 
-LABELS = {0: "pallet", 1: "KKP", 2: "goods"}
+LABELS = {0: "pallet", 1: "pallet", 2: "goods", 3: "storage_cage"}
 
 def draw_contour(img, mask, color):
     contours, _ = cv2.findContours(
@@ -18,20 +17,12 @@ def visulaizeSeg(src_img: np.ndarray, detections):
     for i in range(len(detections)):
         # Extract data for this detection
         bbox = detections.xyxy[i].astype(int)
-        mask = detections.mask[i]
+        # mask = detections.mask[i]
         class_id = detections.class_id[i]
         conf = detections.confidence[i]
 
         # Generate a random color for each instance
         color = [int(c) for c in np.random.randint(0, 255, 3)]
-
-        # --- Draw Mask ---
-        # Create a colored overlay for the mask
-        overlay = src_img.copy()
-        overlay[mask] = color
-        # Blend overlay with original image (alpha = 0.5)
-        cv2.addWeighted(overlay, 0.4, src_img, 0.6, 0, src_img)
-        draw_contour(src_img, mask, (255, 255, 255))
 
         # --- Draw Bounding Box ---
         cv2.rectangle(src_img, (bbox[0], bbox[1]), (bbox[2], bbox[3]), color, 1)
@@ -39,38 +30,21 @@ def visulaizeSeg(src_img: np.ndarray, detections):
         # --- Draw Label ---
         label = f"{LABELS[class_id]}: {conf:.2f}"
         cv2.putText(src_img, label, (bbox[0], bbox[1] - 10), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, color, 2)
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
     return src_img
     
-def get_square(img: Image.Image, crop=True) -> Image.Image:
-    width, height = img.size
-    if width == height:
-        return img
-    if crop:
-        left = round((width - height) / 2)
-        upper = 0
-        right = round((width + height) / 2)
-        lower = height
-
-        return img.crop((left, upper, right, lower))
-    
-    padding_size = round((width - height) / 2)
-    padding = (0, padding_size, 0, padding_size)
-    return ImageOps.expand(img, border=padding, fill="black")
-
 
 # model = RFDETRSegMedium(pretrain_weights="output/checkpoint_best_ema.pth", num_queries=100, num_select=100, num_classes=2)
 # model = RFDETRSegSmall(pretrain_weights="output_s/checkpoint_best_total.pth", num_queries=100)
-model = RFDETRSegMedium(pretrain_weights="output/RFDETRSegMedium/2026.06.19-11:11/checkpoint_best_total.pth", num_queries=100)
+model = RFDETRMedium(pretrain_weights="output/RFDETRMedium/2026-05-25-112715/checkpoint_best_total.pth", num_queries=100)
 
 model.optimize_for_inference()
-image = Image.open("/home/linde/Pictures/download (2).jpeg").convert("RGB")
-image = get_square(image)
-
+image = Image.open("/home/linde/Downloads/image_2.png").convert("RGB")
 detections = model.predict(image, threshold=0.5)
 
-# # 1. Convert PIL image to OpenCV BGR format
+# 1. Convert PIL image to OpenCV BGR format
 image_cv = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+
 image_cv = visulaizeSeg(image_cv, detections)
 
 # 3. Display or save
--- InstanceSegmentation/train.py
+++ InstanceSegmentation/train.py
@@ -1,26 +1,27 @@
 from datetime import datetime
 from rfdetr import RFDETRSegMedium, RFDETRSegLarge, RFDETRSegSmall, RFDETRMedium
-from rfdetr.assets.coco_classes import COCO_CLASSES
-from rfdetr.datasets.aug_config import AUG_AGGRESSIVE
+from rfdetr.datasets.aug_configs import AUG_AGGRESSIVE
 
-model = RFDETRMedium(num_queries=100, num_select=80, num_classes=3)
+model = RFDETRSegMedium(pretrain_weights="output/RFDETRSegMedium/2026.06.18-11:32/checkpoint_best_total.pth",
+                        num_queries=100,  num_classes=3)
+# model = RFDETRSegMedium(num_queries=100, num_classes=3)
 # model = RFDETRSegMedium(num_classes=3)
 
 model_name = model.__class__.__name__
-run_dix = datetime.now().strftime("%Y-%m-%d-%H%M%S")
+run_dix = datetime.now().strftime("%Y.%m.%d-%H:%M")
 
 model.train(
-    dataset_dir="/home/linde/Desktop/Datasets/0518_rfdetr",
+    dataset_dir="/home/linde/Desktop/Datasets/0619_rfdetr",
     num_workers=16,
-    epochs=100, batch_size=4, grad_accum_steps=4,
-    lr=6e-4, lr_scheduler='cosine',  # Enables the Cosine Annealing scheduler
+    epochs=100, batch_size=2, grad_accum_steps=8,
+    lr=3e-3, lr_scheduler='cosine',  # Enables the Cosine Annealing scheduler
     # lr=1e-4,
-    # resolution=504,
+    resolution=504,
     aug_config=AUG_AGGRESSIVE,
     output_dir=f"output/{model_name}/{run_dix}", progress_bar=False,
-    wandb=True,                                         # Enables W&B logging
+    wandb=True,                                  # Enables W&B logging
     project=model_name,                          # Optional: Specify W&B project
-    run=datetime.now().strftime("%Y-%m-%d-%H%M%S")      # Optional: Specify W&B run name
+    run=run_dix                                  # Optional: Specify W&B run name
 )
 
 model.export(opset_version=21, output_dir=f"exported_models/{model_name}/{run_dix}", verbose=False)
